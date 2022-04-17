adcd970374f05ede3298182f1cced9edab777401
Levi Stephen <levisteveen@gmail.com>
2022-04-17 18:44:45 +0100

    Started the List component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from '../APIs/youtube';
+import VideoList from "./VideoList";
 
 class App extends React.Component{
-    onSearchSubmit = term => {
-        youtube.get('/search', {params:{
+    state = { videos: [] };
+
+    onSearchSubmit = async term => {
+        const response = await youtube.get('/search', {params:{
             q: term
             }
         });
+        this.setState({videos: response.data.items});
     };
 
     render(){
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onSearchSubmit} />
+                <VideoList videos = {this.state.videos} />
             </div>
         );
     }
